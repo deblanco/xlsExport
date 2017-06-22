48f34b875eaaaf1f8e05e8ae21a33a0cb9ba22bb
Daniel Blanco <daniel.blanco@meteologica.com>
2017-06-22 09:13:07 +0200

    CSV conversion doneç

--- xls-export.js
+++ xls-export.js
@@ -13,27 +13,41 @@ class xlsExport {
   constructor(data, title = 'Worksheet') {
     this._data = data;
     this._title = title;
+  }
 
-    this.MIME = 'data:application/vnd.ms-excel;base64,';
-    this.TEMPLATE_XLS = `
+  set setData(data) {
+    this._data = data;
+  }
+
+  get getData() {
+    return this._data;
+  }
+
+  exportToXLS(fileName = 'export.xls') {
+    const TEMPLATE_XLS = `
         <html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40">
         <head><!--[if gte mso 9]><xml>
         <x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{title}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml>
         <![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head>
         <body>{table}</body></html>`;
-  }
+    const MIME_XLS = 'data:application/vnd.ms-excel;base64,';
 
-  exportToXLS(fileName = 'export') {
     const parameters = { title: this._title, table: this.objectToTable() };
-    const computeOutput = this.TEMPLATE_XLS.replace(/{(\w+)}/g, (x, y) => parameters[y]);
-    const link = document.createElement('a');
-    link.download = `${fileName}.xls`;
-    link.href = this.MIME + this.toBase64(computeOutput);
-    link.click();
+    const computeOutput = TEMPLATE_XLS.replace(/{(\w+)}/g, (x, y) => parameters[y]);
+
+    this.downloadFile(MIME_XLS + this.toBase64(computeOutput), fileName);
   }
 
-  exportToCSV() {
+  exportToCSV(fileName = 'export.csv') {
+    const MIME_CSV = 'data:attachament/csv,';
+    this.downloadFile(MIME_CSV + encodeURIComponent(this.objectToSemicolons()), fileName);
+  }
 
+  downloadFile(output, fileName) {
+    const link = document.createElement('a');
+    link.download = fileName;
+    link.href = output;
+    link.click();
   }
 
   toBase64(string) {
@@ -45,7 +59,7 @@ class xlsExport {
     const colsHead = `<tr>${Object.keys(this._data[0]).map(key => `<td>${key}</td>`).join('')}</tr>`;
 
     const colsData = this._data.map(obj => [`<tr>
-                ${Object.keys(obj).map(col => `<td>${obj[col]}</td>`)}
+                ${Object.keys(obj).map(col => `<td>${obj[col]}</td>`).join('')}
             </tr>`])
       .join('');
 
@@ -64,16 +78,7 @@ class xlsExport {
                             ]).join(';') // join the row with ';'
                         ]).join('\n'); // end of row
 
-    return `${colsHead}\n
-            ${colsData}`;
-  }
-
-  set setData(data) {
-    this._data = data;
-  }
-
-  get getData() {
-    return this._data;
+    return `${colsHead}\n${colsData}`;
   }
 
 }
